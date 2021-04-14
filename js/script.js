ce4cebfa7e526451bebcbb1c66b77213e90cd0e3
coooonga <paolo.licari.nin@gmail.com>
2021-04-14 11:13:36 +0100

    bailesitos

--- js/script.js
+++ js/script.js
@@ -1,45 +1,39 @@
 // this function
-var bailar = function () {
+console.log('hello there friends!');
+
+var hacerUnBaile = function () {
+
 	var currentClassName = document.getElementById('baile').className;
-	}
-	// changing class name
-	if (currentClassName == 'baile') {
-		document.getElementById('baile').className = 'baile red';
-	} else {
-	 	document.getElementById('baile').className = 'baile';
-	}
 
-var sayMyName = function (name) {
-	alert('My name is: '+name);
-}
-
-
-
+		if (currentClassName == 'baile') {
+			document.getElementById('baile').className = 'baile red';
+	 	} else {
+		document.getElementById('baile').className = 'baile';
+		};
 var car = {
-	make: 'VW',
-	type: 'Polo',
-	color: 'blue',
-	isTurnedOn: false,
-	numberOfWheels: 4,
-	seats: [
-		'seat 1', 
-		'seat 2', 
-		'seat 3', 
-		'seat 4'
-		],
-		turnOn: function () {
-			this.isTurnedOn = true;
-		},
-		fly: function() {
-			alert('fly');
-		},
-		switchCar: function (isOn) {
-			console.log('turn car '+isOn)
-			;
-			if (isOn == true) {
-				this.isTurnedOn = true
-			} else {
-				this.isTurnedOn = false
+			make: 'VW',
+			type: 'Polo',
+			color: 'blue',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: [
+					'seat 1', 
+					'seat 2', 
+					'seat 3', 
+					'seat 4'
+					],
 			};
-
-console.log('hello there friends!')
+function isOn () {
+				this.isTurnedOn = true;
+				};
+var fly = function() {
+				alert('fly');
+				};
+var switchCar = function (isOn) {
+				console.log('turn car '+isOn)};
+				if (isOn == true) {
+					this.isTurnedOn = true
+				} else {
+					this.isTurnedOn = false
+					};
+				};
